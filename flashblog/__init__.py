13ba38a023a59154e2e58fd76fdf2279737d35fb
Timishot <timidotcom58@gmail.com>
2024-01-07 14:54:31 +0100

    made some chnges with configuration

--- flashblog/__init__.py
+++ flashblog/__init__.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://timishot:Timilehin1@localhost:3
 db = SQLAlchemy(app)
 bcrypt =  Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = "info"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
@@ -22,4 +22,10 @@ app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
-from flashblog import routes
+from flashblog.users.route import users
+from flashblog.posts.route import posts
+from flashblog.main.route import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
